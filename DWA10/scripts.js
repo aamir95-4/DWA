37fcfacedbb2eb421cca52c4380aab988f69c164
aamir95-4 <aamirosman@icloud.com>
2024-06-19 14:57:46 +0200

    Completed DWA 10

--- DWA10/scripts.js
+++ DWA10/scripts.js
@@ -1,10 +1,12 @@
 const MAX_NUMBER = 1000;
 const MIN_NUMBER = 0;
-const STEP_AMOUNT = 5;
+const STEP_AMOUNT = 1;
 
 const number = document.querySelector('[data-key="number"]');
 const add = document.querySelector('[data-key="add"]');
 const subtract = document.querySelector('[data-key="subtract"]');
+
+const reset = document.querySelector('[data-key="reset"]');
 
 const subtractHandler = () => {
   const newValue = parseInt(number.value) - STEP_AMOUNT;
@@ -34,3 +36,4 @@ const addHandler = () => {
 
 add.addEventListener("click", addHandler);
 subtract.addEventListener("click", subtractHandler);
+reset.addEventListener("click", () => (number.value = 0));
